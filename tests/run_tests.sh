fe80303b11ad37990b0600229b68c760fa222421
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-07-05 10:03:11 +0600

    compatibility with import.bash

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 _base_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source $_base_dir/vendor/github.com/reconquest/import.bash/import.bash
 
-include vendor/github.com/reconquest/test-runner.bash/test-runner.bash
-include vendor/github.com/reconquest/blank.bash/blank.bash
+:import:source github.com/reconquest/test-runner.bash
+:import:source github.com/reconquest/blank.bash
 
 test-runner:set-local-setup      util/setup.sh
 test-runner:set-local-teardown   util/teardown.sh
