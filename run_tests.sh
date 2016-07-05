e8a75b421a71b9f16bae3b953dee28e31f19801e
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-07-05 10:06:25 +0600

    move tests util files

--- run_tests.sh
+++ run_tests.sh
@@ -8,8 +8,8 @@ source $_base_dir/vendor/github.com/reconquest/import.bash/import.bash
 :import:source github.com/reconquest/test-runner.bash
 :import:source github.com/reconquest/blank.bash
 
-test-runner:set-local-setup      util/setup.sh
-test-runner:set-local-teardown   util/teardown.sh
-test-runner:set-testcases-dir    testcases
+test-runner:set-local-setup      tests/util/setup.bash
+test-runner:set-local-teardown   tests/util/teardown.bash
+test-runner:set-testcases-dir    tests/testcases
 
 test-runner:run "${@}"
